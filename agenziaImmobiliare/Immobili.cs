d8cf3bbad9c05b15d9f89c9006ec6500719ce0ff
Valentina Damiani <valentina.damiani@meteda.it>
2025-01-14 13:21:26 +0100

    Aggiungere i file di progetto.

---
+++ agenziaImmobiliare/Immobili.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agenziaImmobiliare
+{
+    public class Immobili
+    {
+        public string Codice { get; set; }
+        public string Indirizzo { get; set; }
+        public int Cap { get; set; }
+        public string Citta { get; set; }
+        public int Superficie { get; set; }
+
+        public int UtentiInteressati { get; set; }
+        public Immobili(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati=0)
+        {
+            Codice = codice;
+            Indirizzo = indirizzo;
+            Cap = cap;
+            Citta = citta;
+            Superficie = superficie;
+            UtentiInteressati = utentiInteressati;
+        }
+        public void AggiungiImmobile(string codice, string indirizzo, int cap, string citta, int superficie)
+        {
+            Codice = codice;
+            Indirizzo = indirizzo;
+            Cap = cap;
+            Citta = citta;
+            Superficie = superficie;
+        }
+
+        public void AumentoInteressati(int aumentoInteressati)
+        {
+            UtentiInteressati += aumentoInteressati;
+        }
+        public void DiminuzioneInteressati(int diminuzioneInteressati)
+        {
+            UtentiInteressati -= diminuzioneInteressati;
+        }
+        public void StampaInfoImmobile()
+        {
+            Console.WriteLine($"Codice: {Codice}\nIndirizzo: {Indirizzo}\nCap: {Cap}\nCitta: {Citta}\nSuperficie: {Superficie}\nUtenti interessati: {UtentiInteressati}");
+        }
+        public override string ToString()
+        {
+            return $"{Indirizzo}\n {Cap} {Citta} \nmq {Superficie}";
+        }
+        public void ReimpostaSuperficie(int nuovaSuperficie)
+        {
+            Superficie = nuovaSuperficie;
+        }
+
+    }
+
+    public class Box : Immobili
+    {
+        public int PostiAuto { get; set; }
+
+        public Box(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int postiAuto) : base(codice, indirizzo, cap, citta, superficie, utentiInteressati)
+        {
+            PostiAuto = postiAuto;
+        }
+        public void AggiungiBox(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int postiAuto)
+        {
+            Codice = codice;
+            Indirizzo = indirizzo;
+            Cap = cap;
+            Citta = citta;
+            Superficie = superficie;
+            UtentiInteressati = utentiInteressati;
+            PostiAuto = postiAuto;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nPosti auto: {PostiAuto}";
+        }
+    }
+    
+
+    public class Abitazione : Immobili
+    {
+        public int NumeroVani { get; set; }
+        public int NumeroBagni { get; set; }
+
+        public Abitazione(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int numeroVani, int numeroBagni) : base(codice, indirizzo, cap, citta, superficie, utentiInteressati)
+        {
+            NumeroVani = numeroVani;
+            NumeroBagni = numeroBagni;
+        }
+        public void AggiungiAbitazione(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int numeroVani, int numeroBagni)
+        {
+            Codice = codice;
+            Indirizzo = indirizzo;
+            Cap = cap;
+            Citta = citta;
+            Superficie = superficie;
+            NumeroVani = numeroVani;
+            NumeroBagni = numeroBagni;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nNumero vani: {NumeroVani}\nNumero bagni: {NumeroBagni}";
+        }
+    }
+    public class Villa : Immobili
+    {
+        public int SuperficieGiardino { get; set; }
+        
+       
+        public Villa(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int superficieGiardino) : base(codice, indirizzo, cap, citta, superficie, utentiInteressati)
+        {
+            SuperficieGiardino = superficieGiardino;
+
+        }
+        public void AggiungiVilla(string codice, string indirizzo, int cap, string citta, int superficie, int utentiInteressati, int superficieGiardino)
+        {
+            Codice = codice;
+            Indirizzo = indirizzo;
+            Cap = cap;
+            Citta = citta;
+            Superficie = superficie;
+            UtentiInteressati = utentiInteressati;
+            SuperficieGiardino = superficieGiardino;
+
+        }
+
+        public void ReimpostaSuperficie(int nuovaSuperficie, int nuovaSuperficieGiardino)
+        {
+            Superficie = nuovaSuperficie;
+            SuperficieGiardino = nuovaSuperficieGiardino;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nSuperficie giardino: {SuperficieGiardino}";
+        }
+    }
+}
+    
+    
+
+
---
+++ agenziaImmobiliare/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace agenziaImmobiliare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var box = new Box(codice: "b001", indirizzo: "Via Puglia 2", cap: 12345, citta: "Firenze", superficie: 100, postiAuto: 2, utentiInteressati:4);
+            var abitazione = new Abitazione(codice: "a001", indirizzo: "Via Damiani 4", cap: 12345, citta: "San Benedetto del Tronto", superficie: 100, numeroVani: 2, numeroBagni: 3, utentiInteressati:3);
+            var villa = new Villa(codice: "v001", indirizzo: "Via Campania 2", cap: 12345, citta: "Napoli", superficie: 100, superficieGiardino: 20, utentiInteressati:1);
+            
+            var box1 = new Box("b001", "Via Puglia 2", 12345, "Firenze", 100, 2, 4);
+            var abitazione1 = new Abitazione("a001", "Via Damiani 4", 12345, "San Benedetto del Tronto", 100, 2, 3, 3);
+            var villa1 = new Villa("v001", "Via Campania 2", 12345, "Napoli", 100, 20, 1);
+
+           
+            box.AggiungiBox(box1.Codice, box1.Indirizzo, box1.Cap, box1.Citta, box1.Superficie, box1.UtentiInteressati, box1.PostiAuto);
+            abitazione.AggiungiAbitazione(abitazione1.Codice, abitazione1.Indirizzo, abitazione1.Cap, abitazione1.Citta, abitazione1.Superficie, abitazione1.UtentiInteressati, abitazione1.NumeroVani, abitazione1.NumeroBagni);
+            villa.AggiungiVilla(villa1.Codice, villa1.Indirizzo, villa1.Cap, villa1.Citta, villa1.Superficie, villa1.UtentiInteressati, villa1.SuperficieGiardino);
+
+
+        }
+    }
+}
